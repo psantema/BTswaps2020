acd53a810d04934855411a58df0c29e3a59393a1
psantema <35033377+psantema@users.noreply.github.com>
2021-04-16 15:47:36 +0200

    Delete BTswaps2020 directory

--- BTswaps2020/R/aaa.R
+++
@@ -1,7 +0,0 @@
-.onLoad <- function(libname, pkgname){
-  packageStartupMessage("Data and scripts
-  to open the code run:
-  file.edit(paste0(.libPaths(), '/BTswaps2020/data.r'))
-  to open the code for randomisations run:
-  file.edit(paste0(.libPaths(), '/BTswaps2020/random.r'))")
- }
