cbf9208968c90f3af19c20662e0827b491b08d20
psantema <35033377+psantema@users.noreply.github.com>
2021-04-16 15:36:42 +0200

    commit
    First commit.

---
+++ BTswaps2020/R/aaa.R
@@ -0,0 +1,7 @@
+.onLoad <- function(libname, pkgname){
+  packageStartupMessage("Data and scripts
+  to open the code run:
+  file.edit(paste0(.libPaths(), '/BTswaps2020/data.r'))
+  to open the code for randomisations run:
+  file.edit(paste0(.libPaths(), '/BTswaps2020/random.r'))")
+ }
